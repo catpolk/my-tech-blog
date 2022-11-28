851bfeb2490b52de1c2534bae5af582d0c54c46e
Catherine <cpolknytx@gmail.com>
2022-11-28 10:36:05 -0600

    added comment seeds file

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,24 @@
+const { Comment } = require('../models');
+
+const commentData = [
+{
+    user_id: 1,
+    post_id: 1,
+    comment_text: 'This is my comment'
+},
+{
+    user_id: 2,
+    post_id: 2,
+    comment_text: 'What a great post'
+},
+{
+    user_id: 3,
+    post_id: 3,
+    comment_text: 'Great post'
+}
+];
+
+//Create and insert multiple instances in bulk
+const seedComment = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
