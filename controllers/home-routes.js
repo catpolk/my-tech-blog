be3f7bd1b326b571bfe0f88afd5926fe41f49025
Catherine <cpolknytx@gmail.com>
2022-11-29 14:36:06 -0600

    routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,9 +2,8 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 
 // GET all posts for homepage
-router.get('/', async (req, res) => {
-    try {
-      const dbPostData = await Post.findAll({
+router.get('/', (req, res) => {
+      Post.findAll({
         include: [
           { 
             model: Post, 
@@ -49,22 +48,52 @@ router.get('/login', (req, res) => {
 //WHEN I click on an existing blog post
 //THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
 router.get('/post/:id', (req, res) => {
-  try {
-    const dbPostData = await Post.findByPk(req.params.id, {
-      include: [
-        {
-          model: Post, 
-          attributes: [
-            'id',
-            'content',
-            'title',
-            'created_at'
+  Post.findOne({
+    where: {
+      id: params.id
+    },
+    attributes: [
+      'id',
+      'content',
+      'title',
+      'created_at'
+    ],
+    include: [
+            {
+              model: Comment,
+              attributes: [
+                'id',
+                'comment_text',
+                'user_id',
+                'post_id',
+                'created_at' 
+              ],
+              include: {
+                model: User, 
+                attributes: ['username']
+              }
+            },
+            {
+              model: User,
+              attributes: ['username']
+            }
           ]
-        }
-      ]
-    })
-  }
-})
+        }).then(dbPostData => {
+          if (!dbPostData) {
+            res.status(404).json({ message: 'Post not found' });
+            return;
+          }
+          const post = dbPostData.get({ plain: true });
+          console.log(post);
+          res.render('one-post', { post, loggedIn: req.session.loggedIn });
+        }).catch (err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+  });
+
+
+
 
 
 
