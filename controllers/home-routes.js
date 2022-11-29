df072ab556fdffcad400fea477a5659fb7e888d5
Catherine <cpolknytx@gmail.com>
2022-11-29 12:53:47 -0600

    added routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,44 +1,37 @@
 const router = require('express').Router();
-const { User, Task, Message, Gallery, Painting } = require('../models');
-const withAuth = require('../utils/auth');
-const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
 
 // GET all posts for homepage
 router.get('/', async (req, res) => {
     try {
-      const dbPostData = await Post.findAll({});
-  
-      const posts = dbPostData.map((post) =>
-        post.get({ plain: true })
-      );
-      res.render('homepage', {
-        posts,
-        loggedIn: req.session.loggedIn,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
+      const dbPostData = await Post.findAll({
+        include: [
+          { 
+            model: Post, 
+            attributes: ['title', 'created_at'],
+        //     include: { 
+        //       model: User, 
+        //       attributes: ['username'] 
+        //     }
+        // },
+        // { 
+        //   model: User,
+        //   attributes: ['username']
+        // }
+      }]
+});
+  const posts = dbPostData.map((post) => 
+  post.get({ plain: true })
+  );
+
+  res.render('homepage', {
+     posts, loggedIn: req.session.loggedIn 
   });
-
-  //end point to planning
-router.get('/dashboard', async (req, res) => {
-    try {
-      const messageData = await Message.findAll();
-      console.log('yo')
-      console.log(messageData)
-      const messages = messageData.map((message) => message.get({ plain: true }));
-      console.log(messages)
-      res.render('planning', {
-        messages,
-        logged_in: req.session.logged_in
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-
-
+} catch(err) {
+  console.log(err);
+  res.status(500).json(err)
+}
+});
 
   // Login route
 router.get('/login', (req, res) => {
@@ -48,5 +41,31 @@ router.get('/login', (req, res) => {
     }
     res.render('login');
   });
+// Signup route 
+  router.get('/sighup', (req, res) => {
+    res.render('signup');
+  });
+
+//WHEN I click on an existing blog post
+//THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
+router.get('/post/:id', (req, res) => {
+  try {
+    const dbPostData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: Post, 
+          attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at'
+          ]
+        }
+      ]
+    })
+  }
+})
+
+
 
   module.exports = router;
